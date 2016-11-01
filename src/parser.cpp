8a7819dd29d20b1a1c4d7430735c17b81c146a0c
tagener-noisu <op4.renegat@gmail.com>
2016-11-01 06:40:03 +0600

    Move token_stream's members definitions into cpp file

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,26 +1,131 @@
 #include "parser.h"
 
+using namespace JSON;
 //----------------------------------------------------------------------------
-JSON::Array JSON::Parser::_parse_array(Token_stream& ts) {
-	JSON::Array res;
+Parser::Token Parser::Token_stream::get() {
+	char c;
+	c = is.get();
+	if (is.eof()) return Token {EOS};
+	switch (c) {
+		case OB:
+		case OE:
+		case AB:
+		case AE:
+		case ASSIGN:
+		case DELIM:
+			curr_tok = {static_cast<Token_kind>(c)};
+			return curr_tok;
+		case ' ':
+		case '\n':
+		case '\t':
+			while (!is.eof()) {
+				if (c != ' ' || c != '\n' || c != '\t')
+					return get();
+				c = is.get();
+			}
+			throw Unexpected_eos {};
+
+		case 't':
+		case 'f':
+		{
+			std::string s;
+			while (!is.eof()) {
+				if (isalpha(c)) s.push_back(c);
+				else {
+					throw Unexpected_token {tail()};
+				}
+
+				if (s.size() == 4 && s == "true") {
+					curr_tok = {BOOL, true};
+					return curr_tok;
+				}
+				else if (s.size() == 5 &&
+					 s == "false")
+				{
+					curr_tok = {BOOL, false};
+					return curr_tok;
+				}
+				else if (s.length() > 5) {
+					while(s.size() != 0) {
+						is.unget();
+						s.pop_back();
+					}
+					throw Unexpected_token {tail()};
+				}
+				c = is.get();
+			}
+			throw Unexpected_eos {};
+		}
+		case QUOT:
+		{
+			std::string s;
+			while (!is.eof()) { // return on QUOT
+				c = is.get();
+				if (c == '"' && s.back() != '\\') {
+					curr_tok = {STRING, s};
+					return curr_tok;
+				}
+				s.push_back(c);
+			}
+			throw Unterminated_string {};
+		}
+		case '1': case '2': case '3': case '4': case '5':
+		case '6': case '7': case '8': case '9': case '0':
+		{
+			is.putback(c);
+			double d;
+			is >> d;
+			curr_tok = {NUMBER, d};
+			return curr_tok;
+		}
+		case '\377':
+			curr_tok = {EOS};
+			return curr_tok;
+		default:
+			is.putback(c);
+			curr_tok = {UNEXPECTED};
+			return curr_tok;
+	}
+}
+//----------------------------------------------------------------------------
+char const* Parser::Token_stream::tail() {
+	is.unget();
+	if (!line) line = new char[22];
+	raw_read(line+1, 20);
+	line[0] = '>';
+	line[21] = '<';
+
+	is.get();
+	return line;
+}
+//----------------------------------------------------------------------------
+void Parser::Token_stream::raw_read(char* p, int n) {
+	is.read(p, n);
+	int i = n;
+	while (i-- != 0)
+		is.putback(p[i]);
+}
+//----------------------------------------------------------------------------
+Array Parser::_parse_array(Token_stream& ts) {
+	Array res;
 	while (ts.get().kind() != EOS) {
 		auto tok = ts.current();
 		if (tok.kind() == STRING)
-			res.push_back(JSON::Value::make(tok.str()));
+			res.push_back(Value::make(tok.str()));
 		else if (tok.kind() == NUMBER)
-			res.push_back(JSON::Value::make(
+			res.push_back(Value::make(
 				tok.number()));
 		else if (tok.kind() == BOOL)
-			res.push_back(JSON::Value::make(
+			res.push_back(Value::make(
 				bool(tok.number())));
 		else if (tok.kind() == AB) {
 			auto a {_parse_array(ts)};
-			res.push_back(JSON::Value::make(std::move(a)));
+			res.push_back(Value::make(std::move(a)));
 			a.clear();
 		}
 		else if (tok.kind() == OB) {
 			auto o {_parse_object(ts)};
-			res.push_back(JSON::Value::make(std::move(o)));
+			res.push_back(Value::make(std::move(o)));
 			o.clear();
 		}
 		else if (tok.kind() == AE) return res;
@@ -33,8 +138,8 @@ JSON::Array JSON::Parser::_parse_array(Token_stream& ts) {
 	throw Unexpected_eos {};
 }
 //----------------------------------------------------------------------------
-JSON::Object JSON::Parser::_parse_object(Token_stream& ts) {
-	JSON::Object res;
+Object Parser::_parse_object(Token_stream& ts) {
+	Object res;
 	while (ts.get().kind() != EOS) {
 		if (ts.current().kind() == STRING) {
 			auto key = ts.current().str();
@@ -44,19 +149,19 @@ JSON::Object JSON::Parser::_parse_object(Token_stream& ts) {
 
 			auto tok = ts.get();
 			if (tok.kind() == STRING)
-				res[key] = JSON::Value::make(
+				res[key] = Value::make(
 					tok.str());
 			else if (tok.kind() == NUMBER)
-				res[key] = JSON::Value::make(
+				res[key] = Value::make(
 					tok.number());
 			else if (tok.kind() == BOOL)
-				res[key] = JSON::Value::make(
+				res[key] = Value::make(
 					bool(tok.number()));
 			else if (tok.kind() == OB) {
-				res[key] = JSON::Value::make(_parse_object(ts));
+				res[key] = Value::make(_parse_object(ts));
 			}
 			else if (tok.kind() == AB) {
-				res[key] = JSON::Value::make(_parse_array(ts));
+				res[key] = Value::make(_parse_array(ts));
 			}
 			else throw Unexpected_token {ts.tail()};
 
@@ -70,11 +175,11 @@ JSON::Object JSON::Parser::_parse_object(Token_stream& ts) {
 	throw Unexpected_eos {};
 }
 //----------------------------------------------------------------------------
-JSON::Object JSON::Parser::parse(std::istream& is) {
+Object Parser::parse(std::istream& is) {
 	Token_stream ts {is};
 	if (ts.get().kind() == EOS) throw Unexpected_eos {"Stream is empty"};
 	if (ts.current().kind() != OB) throw Unexpected_token {"Expected '{'"};
-	JSON::Object res = _parse_object(ts);
+	Object res = _parse_object(ts);
 	return res;
 }
 //----------------------------------------------------------------------------
