4cc0bf5a8002af6ba936c625c598f4b641a3e413
tagener-noisu <op4.renegat@gmail.com>
2016-11-10 20:40:36 +0600

    Update tests

--- test/test.cpp
+++ test/test.cpp
@@ -4,7 +4,33 @@
 
 #include "../include/json.h"
 
-void test_types() {
+void test_creation() {
+	std::cout << "JSON creation tests\n";
+	using JSON::Value;
+	JSON::Object o;
+
+	o["string"] = Value::make("is a string");
+	assert(o["string"]->type() == Value::Type::STRING);
+	assert(o["string"]->str() == "is a string");
+
+	o["bool"] = Value::make(true);
+	assert(o["bool"]->type() == Value::Type::BOOLEAN);
+	assert(o["bool"]->boolean());
+
+	o["number"] = Value::make(42);
+	assert(o["number"]->type() == Value::Type::NUMBER);
+	assert(o["number"]->number() == 42);
+
+	o["array"] = Value::make(JSON::Array {});
+	assert(o["array"]->type() == Value::Type::ARRAY);
+
+	o["object"] = Value::make(JSON::Object {});
+	assert(o["object"]->type() == Value::Type::OBJECT);
+
+	std::cout << "All tests passed\n------\n";
+}
+
+void test_parsing() {
 	std::ifstream ifs {"test/tests/types.json"};
 	auto json = JSON::parse(ifs);
 	ifs.close();
@@ -36,11 +62,11 @@ void test_types() {
 
 	auto& object = json["content"]->object()["object"];
 	assert(object->type() == Value::Type::OBJECT);
-
-	std::cout << "All tests passed\n";
+	std::cout << "All tests passed\n------\n";
 }
 
 int main() {
-	test_types();
+	test_creation();
+	test_parsing();
 	return EXIT_SUCCESS;
 }
